807b5ce3f60192b2010c844e4ac4dde7046b0202
QUEST-GLOBAL\1038234 <arun.rajanadhan@dellteam.com>
2022-04-12 17:13:50 +0530

    Script change

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -2,7 +2,7 @@
 
 cmd /c "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
 
-$solution = "..\src\HelloWorld\HelloWorld.sln"
+$solution = Join-Path -Path $PSScriptRoot -ChildPath "..\src\HelloWorld\HelloWorld.sln"
 
 cmd /c "`"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MsBuild.exe`" `"$solution`" /t:Restore"
 cmd /c "`"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MsBuild.exe`" `"$solution`" /t:Rebuild"
