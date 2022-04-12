55f42065050edb5df46f13266a09b0de3ce01dbc
QUEST-GLOBAL\1038234 <arun.rajanadhan@dellteam.com>
2022-04-12 17:08:34 +0530

    Added readme and updated build scripts.

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -1,1 +1,8 @@
 # Build Hello World project..
+
+cmd /c "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
+
+$solution = "..\src\HelloWorld\HelloWorld.sln"
+
+cmd /c "`"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MsBuild.exe`" `"$solution`" /t:Restore"
+cmd /c "`"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MsBuild.exe`" `"$solution`" /t:Rebuild"
---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Hello World Sample
+
+Hello World Sample to test Jenkins Webhook.
